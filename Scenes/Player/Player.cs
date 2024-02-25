c873abb81343353a16ee07420438e6cc35e485a8
Dalton <daltonstwilliams@gmail.com>
2024-02-24 18:24:51 -0600

    Reverting accidentally reverted changes from main, IDK how this even happened

--- Scenes/Player/Player.cs
+++ Scenes/Player/Player.cs
@@ -10,63 +10,37 @@ using MVM23.Scripts.AuxiliaryScripts;
 [GlobalClass]
 public partial class Player : CharacterBody2D {
     [Export] public float RunSpeed = 150.0f;
-
-    [ExportGroup("Jump Properties")]
-    [Export] public float ApexGravityVelRange = 5F;
-    // Both of the below are in seconds
-    [Export] public double CoyoteTimeBuffer = 0.1;
     [Export] public double EarlyJumpInputBuffer = 0.2;
-    [Export] public float MaxVerticalVelocity = 150.0f;
-    [Export] public float SuperJumpVelocity = 750f;
-
-    [Export] public double SuperJumpMinChargeTime = 1.00;
     [Export] public double SuperJumpInitBufferLimit = 0.75;
-    public double SuperJumpCurrentBufferTime;
-    public double SuperJumpCurrentChargeTime;
-    public bool CanSuperJump { get; set; }
     
+    [Export] private float _jumpHeight = 70F;  // I believe this is pixels
+    [Export] private float _timeInAir = 0.17F; // No idea what this unit is. Definitely NOT seconds
+    public float Gravity;
+    public float JumpSpeed;
+    public float ApexGravity;
+    
+    public double SuperJumpCurrentBufferTime;
     public double CoyoteTimeElapsed;
     public bool CoyoteTimeExpired;
     //public double EarlyJumpInputCounter;
     //public bool EarlyJumpTimeExpired;
     
-    [ExportSubgroup("Constant Setters")]
-    [Export] private float _jumpHeight = 70F; // I believe this is pixels
-    [Export] private float _timeInAir = 0.17F; // No idea what this unit is. Definitely NOT seconds
-    public float Gravity;
-    public float JumpSpeed;
-    public float ApexGravity;
-
-    [ExportGroup("Dash Properties")]
-    [Export] public float DashDuration = 0.08f;
-    [Export] public double DashSpeed = 750.0f;
-    public double DashTimeElapsed;
-    public Vector2 DashStoredVelocity;
-    public Vector2 DashCurrentAngle;
-
+    public bool CanSuperJump { get; set; }
     private bool IsDashing { get; set; }
     private bool PlayerCanDash { get; set; }
-    public bool IsFacingLeft { get; set; }
+    public IPlayerState CurrentState { get; private set; }
 
     private AnimatedSprite2D _sprite;
+    public bool IsFacingLeft { get; private set; }
     public RayCast2D BonkCheck;
     public RayCast2D BonkBuffer;
     private CpuParticles2D _dashParticles;
     private Node2D _reticle;
-
-    public IPlayerState CurrentState { get; private set; }
     private bool _reticleFrozen; // TODO: control with _currentState method
     private Vector2 _reticleFreezePos;
 
-    public class InputInfo {
-        public Vector2 InputDirection { get; init; }
-        public bool IsPushingJump { get; init; }
-        public bool IsPushingCrouch { get; init; }
-        public bool IsPushingDash { get; init; }
-        public bool IsPushingGrapple { get; init; }
-    }
-
     private PackedScene _grappleScene;
+
     
     public override void _Ready() {
         Gravity = (float)(_jumpHeight / (2 * Math.Pow(_timeInAir, 2)));
@@ -136,6 +110,14 @@ public partial class Player : CharacterBody2D {
         //  It should be able to return something like PlayerState.Previous to go back to whatever the last one was
     }
 
+    public class InputInfo {
+        public Vector2 InputDirection { get; init; }
+        public bool IsPushingJump { get; init; }
+        public bool IsPushingCrouch { get; init; }
+        public bool IsPushingDash { get; init; }
+        public bool IsPushingGrapple { get; init; }
+    }
+
     private static InputInfo GetInputs() {
         var inputInfo = new InputInfo
         {
@@ -191,14 +173,14 @@ public partial class Player : CharacterBody2D {
         _dashParticles.Emitting = emit;
     }
 
-    public float GetAngleToMouse() => GetAngleTo(GetViewport().GetMousePosition());
+    private float GetAngleToMouse() => GetAngleTo(GetViewport().GetMousePosition());
 
-    public void FreezeReticle() {
+    private void FreezeReticle() {
         _reticleFrozen = true;
         _reticleFreezePos = _reticle.GlobalPosition;
     }
 
-    public void RestoreReticle() {
+    private void RestoreReticle() {
         _reticleFrozen = false;
     }
 
