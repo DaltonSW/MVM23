36e204f5294d975b5308dc06d7a015525d8e6277
Dalton <daltonstwilliams@gmail.com>
2024-02-29 19:47:49 -0600

    Small grapple additions

--- Scenes/Abilities/grapple_hook/GrappleHook.cs
+++ Scenes/Abilities/grapple_hook/GrappleHook.cs
@@ -1,17 +1,25 @@
 using Godot;
-using System;
 
 public partial class GrappleHook : Node2D {
-    [Export] private const float Speed = 200f;
-    
+    [Export] public float Speed = 200f;
+    [Export] public double Lifespan = 5;
+    private double _timeAlive;
+
     [Signal]
     public delegate void GrappleHookStruckEventHandler();
 
-    
+
     // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
+    public override void _Ready() { }
+
+    private void OnAreaEntered(Area2D area) {
+        GD.Print($"Grappling hook has intersected with an area: {area}");
+        // EmitSignal(nameof(GrappleHookStruck));
+    }
+
+    private void OnBodyEntered(Node2D body) {
+        GD.Print($"Grappling hook has intersected with a body: {body}");
+        // EmitSignal(nameof(GrappleHookStruck));
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,5 +27,8 @@ public partial class GrappleHook : Node2D {
         var position = Position;
         position += new Vector2(Speed * (float)delta * Mathf.Cos(Rotation), Speed * (float)delta * Mathf.Sin(Rotation));
         Position = position;
+        _timeAlive += delta;
+        if (_timeAlive > Lifespan)
+            QueueFree();
     }
 }
