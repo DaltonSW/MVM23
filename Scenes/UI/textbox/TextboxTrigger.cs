485dfe384b871125c6106b44576312edb03e02a7
Dalton <daltonstwilliams@gmail.com>
2024-03-10 19:42:00 -0500

    E indicator above interaction prompts

--- Scenes/UI/textbox/TextboxTrigger.cs
+++ Scenes/UI/textbox/TextboxTrigger.cs
@@ -1,5 +1,4 @@
 using Godot;
-using System;
 
 public partial class TextboxTrigger : Area2D {
     [Export] public string TextboxID;
@@ -7,17 +6,32 @@ public partial class TextboxTrigger : Area2D {
     private Player _player;
     private PackedScene _textboxScene;
 
+    private AnimatedSprite2D _indicator;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready() {
         _player = GetNode<Player>("../Player");
         _textboxScene = ResourceLoader.Load<PackedScene>("res://Scenes/UI/textbox/textbox.tscn");
+        
+        _indicator = GetNode<AnimatedSprite2D>("Indicator");
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta) {
-        if (Input.IsActionJustPressed("interact") && OverlapsBody(_player)) {
-            SpawnTextbox();
+        if (!OverlapsBody(_player)) {
+            _indicator.Frame = 0;
+            _indicator.Stop();
+            return;
         }
+        
+        if (_indicator.Frame == 0 && !_indicator.IsPlaying())
+            _indicator.Play("fade-in");
+
+        if (!Input.IsActionJustPressed("interact")) return;
+        
+        _indicator.Frame = 0;
+        _indicator.Stop();
+        SpawnTextbox();
     }
 
     private void SpawnTextbox() {
