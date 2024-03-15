f833286f6edc3dad8891b79fa045a1c6c699b402
Dalton <daltonstwilliams@gmail.com>
2024-03-14 21:07:25 -0500

    Main menu & pause menu (#129)
    * Functional main menu
    * Pause menu

--- Scenes/UI/MainMenu/MainMenu.cs
+++ Scenes/UI/MainMenu/MainMenu.cs
@@ -5,7 +5,8 @@ public partial class MainMenu : Control {
     private AudioStreamWav _menuSong;
     private Sprite2D _creditsSprite;
 
-    private Button _startButton;
+    private Button _newGameButton;
+    private Button _loadGameButton;
     private Button _creditsButton;
     private Button _quitButton;
 
@@ -13,9 +14,10 @@ public partial class MainMenu : Control {
         // _menuSong = GD.Load<AudioStreamWav>("res://Assets/Sounds/MainMenu.wav");
         // _audioPlayer = GetNode<AudioStreamPlayer>("AudioPlayer");
         _creditsSprite = GetNode<Sprite2D>("Credits");
-        _startButton = GetNode<Button>("StartButton");
-        _creditsButton = GetNode<Button>("CreditsButton");
-        _quitButton = GetNode<Button>("QuitButton");
+        _newGameButton = GetNode<Button>("HBoxContainer/NewGameButton");
+        _loadGameButton = GetNode<Button>("HBoxContainer/LoadGameButton");
+        _creditsButton = GetNode<Button>("HBoxContainer/CreditsButton");
+        _quitButton = GetNode<Button>("HBoxContainer/QuitButton");
 
         // _audioPlayer.Autoplay = true;
         // _audioPlayer.Stream = _menuSong;
@@ -27,12 +29,13 @@ public partial class MainMenu : Control {
         if (!_creditsSprite.Visible ||
             (!Input.IsActionJustPressed("pause") && !Input.IsActionJustPressed("close_menu"))) return;
         _creditsSprite.Visible = false;
-        _startButton.Disabled = false;
+        _newGameButton.Disabled = false;
+        _loadGameButton.Disabled = false;
         _creditsButton.Disabled = false;
         _quitButton.Disabled = false;
     }
 
-    private void _on_StartButton_pressed() {
+    private void _on_NewGameButton_pressed() {
         GetTree().ChangeSceneToFile("res://Scenes/Levels/Game.tscn");
     }
 
@@ -42,7 +45,8 @@ public partial class MainMenu : Control {
 
     private void _on_CreditsButton_pressed() {
         _creditsSprite.Visible = true;
-        _startButton.Disabled = true;
+        _newGameButton.Disabled = true;
+        _loadGameButton.Disabled = true;
         _creditsButton.Disabled = true;
         _quitButton.Disabled = true;
     }
