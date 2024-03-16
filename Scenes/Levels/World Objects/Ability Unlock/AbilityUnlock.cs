5b1f3a30c65a5590151c43c362f6a204e4327696
Dalton <daltonstwilliams@gmail.com>
2024-03-15 20:00:52 -0500

    Ability tutorials (#149)
    * Rename .yml to .yaml
    
    * Ability tutorial dialog
    
    * Ability tutorials

--- Scenes/Levels/World Objects/Ability Unlock/AbilityUnlock.cs
+++ Scenes/Levels/World Objects/Ability Unlock/AbilityUnlock.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using MVM23;
 
 public partial class AbilityUnlock : Area2D
@@ -7,13 +8,24 @@ public partial class AbilityUnlock : Area2D
     [Export(PropertyHint.Enum, "Stick,Dash,SuperJump,Grapple,DoubleDash,DashOnKill,KeyToWorldTwo,WorldTwoBossKey,WorldThreeKeyOne,WorldThreeKeyTwo")] 
     private string _myUnlock;
 
+    private List<string> _tutorial = new()
+    {
+        "Stick",
+        "Dash",
+        "SuperJump",
+        "Grapple",
+        "DoubleDash"
+    };
+
     private Player _player;
+    private PackedScene _textboxScene;
     private WorldStateManager _worldStateManager;
     
     // Called when the node enters the scene tree for the first time.
     public override void _Ready() {
         _player = GetNode<Player>("/root/Game/Player");
         _worldStateManager = GetNode<WorldStateManager>("/root/Game/WSM");
+        _textboxScene = ResourceLoader.Load<PackedScene>("res://Scenes/UI/textbox/textbox.tscn");
         
         if (_worldStateManager.IsObjectActivated(_myUnlock)) QueueFree();
     }
@@ -25,5 +37,14 @@ public partial class AbilityUnlock : Area2D
         _player.UnlockAbility(_myUnlock);
         _worldStateManager.SetObjectAsActivated(_myUnlock);
         QueueFree();
+        if (_tutorial.Contains(_myUnlock))
+            SpawnTextbox();
+    }
+    
+    private void SpawnTextbox() {
+        var textbox = _textboxScene.Instantiate<Textbox>();
+        textbox.DialogueID = _myUnlock;
+        GetParent().AddChild(textbox);
+        GetTree().Paused = true;
     }
 }
